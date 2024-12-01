18c45acd25523b2d27a92f11739b3a8f552f640a
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2024-12-01 09:09:29 -0300

    add `sonner alerts`

--- pages/_app.public.js
+++ pages/_app.public.js
@@ -1,10 +1,12 @@
-import { UserProvider } from "pages/interface";
+import { Toaster } from "sonner";
 
+import { UserProvider } from "pages/interface";
 import "components/css/index.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Toaster />
       <Component {...pageProps} />
     </UserProvider>
   );
--- pages/admin/ingredients/create/index.public.js
+++ pages/admin/ingredients/create/index.public.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 import { useUser } from "pages/interface";
 import AdminHeader from "components/adminHeader";
@@ -17,6 +18,12 @@ export default function CreateIngredient() {
       router.push(`/login`);
     }
   }, [user, router, isLoading]);
+
+  function clearInputs() {
+    setName("");
+    setValue("");
+    setPrice("");
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,11 +44,17 @@ export default function CreateIngredient() {
       });
 
       if (res.status == 201) {
-        alert("Ingrediente criado com sucesso");
-        location.reload();
+        toast.success("Ingrediente criado com sucesso!", {
+          className: "alert success",
+          duration: 2000,
+        });
+        clearInputs();
       } else {
         const resBody = await res.json();
-        alert(resBody.message);
+        toast.error(resBody.message, {
+          className: "alert error",
+          duration: 2000,
+        });
       }
     } catch (err) {
       console.error("Error submiting form: ", err);
